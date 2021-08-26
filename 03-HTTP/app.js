814a7ca6f13a54588ab2d028253aa4793417aafd
Victor Kauan <victorkauan.alu.lmb@gmail.com>
2021-08-26 15:53:04 -0300

    Node HTTP module

---
+++ 03-HTTP/app.js
@@ -0,0 +1,9 @@
+let http = require("http");
+
+http
+  .createServer(function(req, res) {
+    res.end("Hello, world! Welcome to my website!");
+  })
+  .listen(1300);
+
+console.log("Server is running on port 1300!");
